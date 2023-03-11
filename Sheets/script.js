59c82b51b2bd2d202b1c9b956bd9d5aa6d99ac79
Z-Olek <36603575+Olek97@users.noreply.github.com>
2023-03-11 15:45:13 +0100

    Updates

--- Sheets/script.js
+++ Sheets/script.js
@@ -1,3 +1,14 @@
+var xhr = new XMLHttpRequest();
+xhr.open("HEAD", window.location.href, true);
+xhr.onreadystatechange = function() {
+    if (this.readyState === this.DONE) {
+        var lastModified = new Date(xhr.getResponseHeader("Last-Modified"));
+        var p = document.getElementById("last-update");
+        p.innerHTML = "Last update " + lastModified.toLocaleDateString();
+    }
+};
+xhr.send();
+
 window.addEventListener("load", function() {
     document.querySelectorAll('link[rel="stylesheet"], img, script').forEach(function(el) {
       el.setAttribute("cache-control", "no-cache, no-store, must-revalidate");
