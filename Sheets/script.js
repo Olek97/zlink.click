66a62e3c9a349d3b827c67895c44e726d96b1e0c
Olek97 <36603575+Olek97@users.noreply.github.com>
2023-03-12 15:44:04 +0100

    Implementions
    - Added blockchair API.
    - Added halving countdown.

--- Sheets/script.js
+++ Sheets/script.js
@@ -1,21 +1,58 @@
-var xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 xhr.open("HEAD", window.location.href, true);
-xhr.onreadystatechange = function() {
-    if (this.readyState === this.DONE) {
-        var lastModified = new Date(xhr.getResponseHeader("Last-Modified"));
-        var p = document.getElementById("last-update");
-        p.innerHTML = "Last update " + lastModified.toLocaleDateString();
-    }
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === xhr.DONE) {
+    const lastModified = new Date(xhr.getResponseHeader("Last-Modified"));
+    document.getElementById("last-update").innerHTML = `Site updated on ${lastModified.toLocaleDateString()}`;
+  }
 };
 xhr.send();
 
-window.addEventListener("load", function() {
-    document.querySelectorAll('link[rel="stylesheet"], img, script').forEach(function(el) {
-      el.setAttribute("cache-control", "no-cache, no-store, must-revalidate");
-      el.setAttribute("pragma", "no-cache");
-      el.setAttribute("expires", "0");
-    });
+window.addEventListener("load", function () {
+  document.querySelectorAll('link[rel="stylesheet"], img, script').forEach(function (el) {
+    el.setAttribute("cache-control", "no-cache, no-store, must-revalidate");
+    el.setAttribute("pragma", "no-cache");
+    el.setAttribute("expires", "0");
   });
+});
 
-const MaxSupply = 21000000;
-const BlocksPerDay = 1152;
+const url = "https://api.blockchair.com/zcash/stats";
+
+const maxSupply = 21000000;
+const blocksPerDay = 1152;
+const halvingBlock = 2726400;
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const blockHeight = data.data.blocks;
+    const blocksToHalving = halvingBlock - blockHeight;
+    const timeToHalving = blocksToHalving * 75;
+
+    let countDownDate = new Date().getTime() + (timeToHalving * 1000);
+    let pTag = document.getElementById("big-timer");
+
+    function updateTimer() {
+      let now = new Date().getTime();
+      let distance = countDownDate - now;
+
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24)).toString().padStart(2, "0");
+      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, "0");
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, "0");
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000).toString().padStart(2, "0");
+
+      pTag.innerHTML = `Time to halving: ${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+      if (distance < 0) {
+        clearInterval(x);
+        pTag.innerHTML = "Remember to @Olek97 on telegram to update the countdown.";
+      } else {
+        requestAnimationFrame(updateTimer);
+      }
+    }
+
+    updateTimer();
+  })
+  .catch((error) => console.log(error));
+
+
