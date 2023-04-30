f4ac8a9f4c6bee1bf6b963ee9ccebb80cefe17f1
Olek <36603575+Olek97@users.noreply.github.com>
2023-04-30 22:35:24 +0200

    Update
    - Updated translations to the latest version.
    - Translated newsletter #40.
    - Wrote an article about Namada's airdrop.

--- sheets/scriptV06.js
+++ sheets/scriptV06.js
@@ -15,9 +15,17 @@ xhr.onreadystatechange = () => {
 };
 xhr.send();
 
-// Footer copy address
-function copyAddr() {
-  const addr = document.querySelector("#copy-addr").innerText;
-  navigator.clipboard.writeText(addr);
-  alert("Thank you!");
+// Collapse
+const coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
