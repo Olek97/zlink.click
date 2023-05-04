80ee21eb278182de9a8320130dde5ce950544344
Olek <36603575+Olek97@users.noreply.github.com>
2023-05-04 21:06:27 +0200

    Collapse it! :)

--- sheets/scriptV06.js
+++ sheets/scriptV06.js
@@ -16,16 +16,21 @@ xhr.onreadystatechange = () => {
 xhr.send();
 
 // Collapse
-const coll = document.getElementsByClassName("collapsible");
+const collapsibles = document.querySelectorAll('.collapsible');
+const contents = document.querySelectorAll('.content');
 
-for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    const content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+collapsibles.forEach((collapsible, index) => {
+  collapsible.addEventListener('click', () => {
+    contents.forEach((content, contentIndex) => {
+      if (contentIndex !== index) {
+        content.style.display = 'none';
+      }
+    });
+    
+    if (contents[index].style.display !== 'block') {
+      contents[index].style.display = 'block';
     } else {
-      content.style.display = "block";
+      contents[index].style.display = 'none';
     }
   });
-}
+});
