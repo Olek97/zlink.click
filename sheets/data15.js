f9fe0df133e58a10210179c06361af739c7e52e6
Olek <36603575+Olek97@users.noreply.github.com>
2024-05-27 23:26:45 +0200

    Fix

--- sheets/data15.js
+++ sheets/data15.js
@@ -1,22 +1,22 @@
-const innHTML = {
-  innPrice: document.querySelector("#txt-price"),
-  innMarketCap: document.querySelector("#txt-market-cap"),
-  innDiluitedMarketCap: document.querySelector("#txt-diluited-market-cap"),
-  innPriceChange24: document.querySelector("#txt-price-change24"),
-  innCurrentBlock: document.querySelector("#txt-current-block"),
-  innToHalving: document.querySelector("#txt-to-halving"),
-  innBlockReward: document.querySelector("#txt-block-reward"),
-  innDailyZec: document.querySelector("#txt-daily-zec"),
-  innInflation: document.querySelector("#txt-inflation"),
-  innDifficulty: document.querySelector("#txt-difficulty"),
-  innTimer: document.querySelector("#txt-timer"),
-  innHalvingDate: document.querySelector("#txt-halving-date"),
-  innMempool: document.querySelector("#txt-mempool"),
-  innTransparentPool: document.querySelector("#txt-tpool"),
-  innSproutPool: document.querySelector("#txt-zcpool"),
-  innSaplingPool: document.querySelector("#txt-zspool"),
-  innOrchardPool: document.querySelector("#txt-opool"),
-  innTotalSupply: document.querySelector("#txt-total-supply"),
+const domElements = {
+  price: document.querySelector("#txt-price"),
+  marketCap: document.querySelector("#txt-market-cap"),
+  diluitedMarketCap: document.querySelector("#txt-diluited-market-cap"),
+  priceChange24: document.querySelector("#txt-price-change24"),
+  currentBlock: document.querySelector("#txt-current-block"),
+  toHalving: document.querySelector("#txt-to-halving"),
+  blockReward: document.querySelector("#txt-block-reward"),
+  dailyZec: document.querySelector("#txt-daily-zec"),
+  inflation: document.querySelector("#txt-inflation"),
+  difficulty: document.querySelector("#txt-difficulty"),
+  timer: document.querySelector("#txt-timer"),
+  halvingDate: document.querySelector("#txt-halving-date"),
+  mempool: document.querySelector("#txt-mempool"),
+  transparentPool: document.querySelector("#txt-tpool"),
+  sproutPool: document.querySelector("#txt-zcpool"),
+  saplingPool: document.querySelector("#txt-zspool"),
+  orchardPool: document.querySelector("#txt-opool"),
+  totalSupply: document.querySelector("#txt-total-supply"),
 };
 
 const previousHalvingBlock = 1046400;
@@ -29,7 +29,7 @@ const dailyZec = blocksPerDay * blockReward;
 const yearlyZec = blocksPerYear * blockReward;
 const inflationRate = (yearlyZec / maxSupply) * 100;
 
-let transparent, sprout, sapling, orchard, totalSupply, currentBlock, difficulty, mempool, blocksToHalving, secsToHalving, countDownDate, price;
+let transparent, sprout, sapling, orchard, totalSupply, currentBlock, difficulty, mempool, blocksToHalving, secsToHalving, countDownDate, price, priceChange24, marketCap, diluitedMarketCap;
 
 const api = [
   "https://api.blockchair.com/zcash/stats",
@@ -37,14 +37,12 @@ const api = [
   "https://api.coingecko.com/api/v3/simple/price?ids=zcash&vs_currencies=usd&include_market_cap=false&include_24hr_vol=false&include_24hr_change=true&include_last_updated_at=false&precision=5&c"
 ];
 
-function fetchData(url) {
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`API request error: ${response.status} ${response.statusText}`);
-      }
-      return response.json();
-    });
+async function fetchData(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`API request error: ${response.status} ${response.statusText}`);
+  }
+  return response.json();
 }
 
 async function fetchAllData() {
@@ -63,9 +61,9 @@ async function fetchAllData() {
     orchard = zbe.valuePools[3].chainValue;
     totalSupply = transparent + sprout + sapling + orchard;
     price = cgk.zcash.usd;
-    const priceChange24 = cgk.zcash.usd_24h_change;
-    const marketCap = price * totalSupply;
-    const diluitedMarketCap = price * maxSupply;
+    priceChange24 = cgk.zcash.usd_24h_change;
+    marketCap = price * totalSupply;
+    diluitedMarketCap = price * maxSupply;
 
     updateDOM({
       countDownDate,
@@ -106,23 +104,23 @@ async function fetchAllData() {
 }
 
 function updateDOM(data) {
-  innHTML.innHalvingDate.innerHTML = new Date(data.countDownDate).toDateString();
-  innHTML.innPrice.innerHTML = data.price.toFixed(2) + " $";
-  innHTML.innMarketCap.innerHTML = Math.floor(data.marketCap).toLocaleString("en-US") + " $";
-  innHTML.innDiluitedMarketCap.innerHTML = Math.floor(data.diluitedMarketCap).toLocaleString("en-US") + " $";
-  innHTML.innBlockReward.innerHTML = blockReward.toLocaleString("en-US") + " ZEC";
-  innHTML.innDailyZec.innerHTML = dailyZec.toLocaleString("en-US") + " ZEC";
-  innHTML.innInflation.innerHTML = inflationRate.toFixed(2) + "%";
-  innHTML.innToHalving.innerHTML = data.blocksToHalving.toLocaleString("en-US");
-  innHTML.innDifficulty.innerHTML = data.difficulty.toLocaleString("en-US");
-  innHTML.innCurrentBlock.innerHTML = data.currentBlock.toLocaleString("en-US");
-  innHTML.innMempool.innerHTML = data.mempool.toLocaleString("en-US") + " TXs";
-  innHTML.innTransparentPool.innerHTML = Math.floor(data.transparent).toLocaleString("en-US") + " ZEC";
-  innHTML.innSproutPool.innerHTML = Math.floor(data.sprout).toLocaleString("en-US") + " ZEC";
-  innHTML.innSaplingPool.innerHTML = Math.floor(data.sapling).toLocaleString("en-US") + " ZEC";
-  innHTML.innOrchardPool.innerHTML = Math.floor(data.orchard).toLocaleString("en-US") + " ZEC";
-  innHTML.innTotalSupply.innerHTML = Math.floor(data.totalSupply).toLocaleString("en-US") + " ZEC";
-  innHTML.innPriceChange24.innerHTML = data.priceChange24.toFixed(2) + "%";
+  domElements.halvingDate.innerHTML = new Date(data.countDownDate).toDateString();
+  domElements.price.innerHTML = data.price.toFixed(2) + " $";
+  domElements.marketCap.innerHTML = Math.floor(data.marketCap).toLocaleString("en-US") + " $";
+  domElements.diluitedMarketCap.innerHTML = Math.floor(data.diluitedMarketCap).toLocaleString("en-US") + " $";
+  domElements.blockReward.innerHTML = blockReward.toLocaleString("en-US") + " ZEC";
+  domElements.dailyZec.innerHTML = dailyZec.toLocaleString("en-US") + " ZEC";
+  domElements.inflation.innerHTML = inflationRate.toFixed(2) + "%";
+  domElements.toHalving.innerHTML = data.blocksToHalving.toLocaleString("en-US");
+  domElements.difficulty.innerHTML = data.difficulty.toLocaleString("en-US");
+  domElements.currentBlock.innerHTML = data.currentBlock.toLocaleString("en-US");
+  domElements.mempool.innerHTML = data.mempool.toLocaleString("en-US") + " TXs";
+  domElements.transparentPool.innerHTML = Math.floor(data.transparent).toLocaleString("en-US") + " ZEC";
+  domElements.sproutPool.innerHTML = Math.floor(data.sprout).toLocaleString("en-US") + " ZEC";
+  domElements.saplingPool.innerHTML = Math.floor(data.sapling).toLocaleString("en-US") + " ZEC";
+  domElements.orchardPool.innerHTML = Math.floor(data.orchard).toLocaleString("en-US") + " ZEC";
+  domElements.totalSupply.innerHTML = Math.floor(data.totalSupply).toLocaleString("en-US") + " ZEC";
+  domElements.priceChange24.innerHTML = data.priceChange24.toFixed(2) + "%";
 }
 
 function startTimer() {
@@ -139,7 +137,7 @@ function startTimer() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, "0");
     const seconds = Math.floor((distance % (1000 * 60)) / 1000).toString().padStart(2, "0");
 
-    innHTML.innTimer.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s to go`;
+    domElements.timer.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s to go`;
   }, 1000);
 }
 
